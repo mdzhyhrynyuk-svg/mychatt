80dbc61d173897a5e64031931637b51d5cad17c7
mdzhyhrynyuk-svg <m.dzhyhrynyuk@gmail.com>
2026-05-29 21:54:50 +0200

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-window.ERMI_SIGNAL_SERVER = "https://army-chat-server.onrender.com";
+window.ERMI_SIGNAL_SERVER = "https://ermi-server.onrender.com";
 window.ARMY_CHAT_SIGNAL_SERVER = window.ERMI_SIGNAL_SERVER;
